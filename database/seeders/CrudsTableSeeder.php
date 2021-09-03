167b55357e61b1ebba995504f742b6f6f2255398
HosseinKalateh <hosseinkalateh77@gmail.com>
2021-09-03 10:56:57 +0430

    add CrudsTableSeeder for seed default cruds - admin panel

---
+++ database/seeders/CrudsTableSeeder.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Carbon\Carbon;
+
+class CrudsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+    	// Category CRUD
+    	if (\DB::table('cruds')->where('model', 'Category')->doesntExist()) 
+    	{
+    		\DB::table('cruds')->insert(
+	        	[
+	        	'name'       => 'category',
+	        	'model'      => 'Category',
+	        	'route'      => 'categories',
+	        	'active'     => true,
+	        	'built'      => true,
+	        	'created_at' => Carbon::now(),
+	        	'updated_at' => Carbon::now()
+	        	]       	  
+    		);
+    	}
+        
+        // Author CRUD
+    	if (\DB::table('cruds')->where('model', 'Author')->doesntExist())
+    	{
+    		\DB::table('cruds')->insert(
+	    		[
+	        	'name'       => 'author',
+	        	'model'      => 'Author',
+	        	'route'      => 'authors',
+	        	'active'     => true,
+	        	'built'      => true,
+	        	'created_at' => Carbon::now(),
+	        	'updated_at' => Carbon::now()
+	        	]
+    		);
+    	} 
+    		
+    	// Translator CRUD
+    	if (\DB::table('cruds')->where('model', 'Translator')->doesntExist())
+		{
+			\DB::table('cruds')->insert(
+	    		[
+	        	'name'       => 'translator',
+	        	'model'      => 'Translator',
+	        	'route'      => 'translators',
+	        	'active'     => true,
+	        	'built'      => true,
+	        	'created_at' => Carbon::now(),
+	        	'updated_at' => Carbon::now()
+	        	]
+    		);
+		}
+    	
+    	// Publisher CRUD
+		if (\DB::table('cruds')->where('model', 'Publisher')->doesntExist())
+		{
+			\DB::table('cruds')->insert(
+	    		[
+	        	'name'       => 'publisher',
+	        	'model'      => 'Publisher',
+	        	'route'      => 'publishers',
+	        	'active'     => true,
+	        	'built'      => true,
+	        	'created_at' => Carbon::now(),
+	        	'updated_at' => Carbon::now()
+	        	],
+    		);
+		}
+    	
+    	// Book CRUD
+		if (\DB::table('cruds')->where('model', 'Book')->doesntExist())
+		{
+			\DB::table('cruds')->insert(
+	    		[
+	        	'name'       => 'book',
+	        	'model'      => 'Book',
+	        	'route'      => 'books',
+	        	'active'     => true,
+	        	'built'      => true,
+	        	'created_at' => Carbon::now(),
+	        	'updated_at' => Carbon::now()
+	        	],
+    		);
+		}
+    	
+    }
+}
