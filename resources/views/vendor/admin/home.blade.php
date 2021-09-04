540f8140e597050b968234fde79d22bfd9af7671
HosseinKalateh <hosseinkalateh77@gmail.com>
2021-09-04 16:19:41 +0430

    change admin panel home page content - admin panel

--- resources/views/vendor/admin/home.blade.php
+++ resources/views/vendor/admin/home.blade.php
@@ -1,19 +1,14 @@
 @component('admin::layouts.app')
-    <p>You can edit & customize this page in <code>resources/views/vendor/admin/home.blade.php</code></p>
-
+    
+    <h2>Bookange</h2>
     <h3>Features :</h3>
     <ul>
-        <li>Create CRUD in config file.</li>
-        <li>Manage route prefix and addresses</li>
-        <li>Beautiful UI/UX with AdminMart template</li>
-        <li>Add/remove an admin with command line</li>
-        <li>Manage pagination count</li>
-        <li>Real time validation with <a href="https://github.com/livewire/livewire">Livewire</a></li>
-        <li>Customize every actions in your project</li>
-        <li>A small and beautiful TODO (You disable it in your config)</li>
-        <li>Create a nice and responsive view based on your data in config file for every CRUDs</li>
-        <li>Custom validation based on config file</li>
+        <li>Category CRUD</li>
+        <li>Author CRUD</li>
+        <li>Translator CRUD</li>
+        <li>Publisher CRUD</li>
+        <li>Book CRUD</li>
     </ul>
 
-    <p>Check document out : <a href="https://github.com/rezaamini-ir/laravel-easypanel">Github repo</a></p>
+    <p>Check document out : <a href="https://github.com/HosseinKalateh/Bookange">Github repo</a></p>
 @endcomponent
