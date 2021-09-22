c9b4c9c39edadd5a709aee9e5f462b1217322163
HosseinKalateh <hosseinkalateh77@gmail.com>
2021-09-22 17:20:09 +0330

    Author model test/author relationships - test

--- tests/Feature/Models/AuthorTest.php
+++ tests/Feature/Models/AuthorTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature\Models;
 
 use App\Models\Author;
+use App\Models\Book;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -22,5 +23,21 @@ class AuthorTest extends TestCase
 
         $this->assertDatabaseHas('authors', $data);
         $this->assertDatabaseCount('authors', 1);
+    }
+
+    /**
+     * @test
+     */
+    public function author_relationship_with_book()
+    {
+        $count = rand(1, 10);
+
+        $author = Author::factory()
+                    ->hasBooks($count)
+                    ->create();
+
+        $this->assertTrue(isset($author->books->first()->id));
+        $this->assertTrue($author->books->first() instanceof Book);
+        $this->assertCount($count, $author->books);
     }
 }
