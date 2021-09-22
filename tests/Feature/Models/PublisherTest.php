ffb1e28e420787f94b804b8aa210eac8983f9e3b
HosseinKalateh <hosseinkalateh77@gmail.com>
2021-09-22 19:48:10 +0330

    Publisher model test/publisher relationships - test

--- tests/Feature/Models/PublisherTest.php
+++ tests/Feature/Models/PublisherTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Models;
 
+use App\Models\Book;
 use App\Models\Publisher;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -22,5 +23,21 @@ class PublisherTest extends TestCase
 
         $this->assertDatabaseHas('publishers', $data);
         $this->assertDatabaseCount('publishers', 1);
+    }
+
+    /**
+     * @test
+     */
+    public function publisher_relationship_with_book()
+    {
+        $count = rand(1, 10);
+
+        $publisher = Publisher::factory()
+                                ->hasBooks($count)
+                                ->create();
+
+        $this->assertTrue(isset($publisher->books->first()->id));
+        $this->assertTrue($publisher->books->first() instanceof Book);
+        $this->assertCount($count, $publisher->books);
     }
 }
