bc2c4614c8edae9e3a9d271be89c6e418b6b78d2
HosseinKalateh <hosseinkalateh77@gmail.com>
2021-09-22 19:43:21 +0330

    Translator model test/translator relationships - test

--- tests/Feature/Models/TranslatorTest.php
+++ tests/Feature/Models/TranslatorTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Models;
 
+use App\Models\Book;
 use App\Models\Translator;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -22,5 +23,21 @@ class TranslatorTest extends TestCase
 
         $this->assertDatabaseHas('translators', $data);
         $this->assertDatabaseCount('translators', 1);
+    }
+
+    /**
+     * @test
+     */
+    public function translator_relationship_with_book()
+    {
+        $count = rand(1, 10);
+
+        $translator = Translator::factory()
+                                ->hasBooks($count)
+                                ->create();
+
+        $this->assertTrue(isset($translator->books->first()->id));
+        $this->assertTrue($translator->books->first() instanceof Book);
+        $this->assertCount($count, $translator->books);
     }
 }
