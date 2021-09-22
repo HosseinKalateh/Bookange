396aad339b182999aa53bf02519e131e3d2bf69e
HosseinKalateh <hosseinkalateh77@gmail.com>
2021-09-22 17:14:58 +0330

    Category model test/category relationships

--- tests/Feature/Models/CategoryTest.php
+++ tests/Feature/Models/CategoryTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Models;
 
+use App\Models\Book;
 use App\Models\Category;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -21,5 +22,21 @@ class CategoryTest extends TestCase
 
         $this->assertDatabaseHas('categories', $data);
         $this->assertDatabaseCount('categories', 1);
+    }
+
+    /**
+     * @test
+     */
+    public function category_relationship_with_book()
+    {
+        $count = rand(1, 10);
+
+        $category = Category::factory()
+                            ->hasBooks($count)
+                            ->create();
+
+        $this->assertTrue(isset($category->books->first()->id));
+        $this->assertTrue($category->books->first() instanceof Book);
+        $this->assertCount($count, $category->books);
     }
 }
